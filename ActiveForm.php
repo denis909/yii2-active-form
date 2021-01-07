99660fb5c6d9f16b85666ad80ad30291e233aeb7
denis909 <denis909@mail.ru>
2021-01-07 10:40:26 +0300

    n/a

---
+++ ActiveForm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace denis909\yii;
+
+class ActiveForm extends \yii\widgets\ActiveForm
+{
+
+    public $enableAjaxValidation = false;
+
+    public $enableClientValidation = false;
+
+    public $enableClientScript = false;
+
+}
